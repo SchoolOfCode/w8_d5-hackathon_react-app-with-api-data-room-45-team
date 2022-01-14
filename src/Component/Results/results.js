79a6e10e2b93d5fc5b16f95bc79cf9927621ef68
ashliecodes <ashlieprempeh@hotmail.co.uk>
2022-01-14 11:13:28 +0000

    starting to fetch data using async function

--- src/Component/Results/results.js
+++ src/Component/Results/results.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Forecast from "./Forecast/forecast";
 import Today from "./Today/today";
 
 function Results() {
+  //   const [weather, setWeather] = useState("");
+  //   useEffect(() => {
+  //     async function getWeather() {
+  //       const response = await fetch(
+  //         "https://goweather.herokuapp.com/weather/London"
+  //       );
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setWeather([data]);
+  //     }
+  //     getWeather();
+  //   }, []);
 
-    async function 
-
-    return (
+  return (
     <div>
       <Forecast />
+      <h1>Hello World</h1>
       <Today />
     </div>
   );
