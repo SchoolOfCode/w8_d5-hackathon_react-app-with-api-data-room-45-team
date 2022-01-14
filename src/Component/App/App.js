6be605617514e760001cee37792c405eb313da16
Jacques Everiss <jacqueseveriss@jacquess-mbp-2.home>
2022-01-14 10:45:55 +0000

    files made

---
+++ src/Component/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Search from "../Search/search";
+import Results from "../Results/results";
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+
+      <Results />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Results/results.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Forecast from "./Forecast/forecast";
+import Today from "./Today/today";
+
+function Results() {
+
+    async function 
+
+    return (
+    <div>
+      <Forecast />
+      <Today />
+    </div>
+  );
+}
+
+export default Results;
